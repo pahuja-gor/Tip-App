87e707a377599dc564752adc01b77df787053a12
Goral Pahuja <pahuja.gor@gmail.com>
2020-07-22 15:31:45 -0400

    Adding the SliderUI Object to allow the User to select a Tip Percentage between 0% and 100%. Added documentation to the functions as well.

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // Attaches/Declares the UI Fields
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipPercentageLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+//    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipPercentageSlider: UISlider!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var numPeopleTextField: UITextField!
     @IBOutlet weak var totalPerPersonLabel: UILabel!
@@ -23,57 +25,42 @@ class ViewController: UIViewController {
     }
     
     @IBAction func onTap(_ sender: Any) {
-//        view.endEditing(false)
+        // Functionality for the Tap Gesture Recognizer object
     }
     
-//    func retrieveOriginalTotal() -> Double {
-//        return Double(totalLabel.text!) ?? 0.00
-//    }
-    
     @IBAction func calculateTip(_ sender: Any) {
+        /**
+         Calculates the tip and total using the "Bill" value from the Text Field object and the "Tip Percentage" value from the Slider object.
+         */
         
         // Get the bill amount
         let bill = Double(billAmountTextField.text!) ?? 0
-        let tipPercentages = [0.15, 0.18, 0.20]
+        let tipPercentage = Double(Int(tipPercentageSlider.value)) / 100.0
+        //        let tipPercentages = [0.15, 0.18, 0.20]
+        
+        tipPercentageLabel.text = String(format: "%i%%", Int(tipPercentageSlider.value))
         
         // Calculate the tip and total
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tip = bill * Double(tipPercentage)
+        //        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
         // Update the tip and total labels
-        tipPercentageLabel.text = String(format: "%.2f", tip)
-        totalLabel.text = String(format: "%.2f", total)
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
         
     }
     
     @IBAction func splitBill(_ sender: Any) {
-        let preSplitTotal = Double(totalLabel.text!) ?? 0
+        /**
+         Splits the total bill by the number of people attending the restaurant, in order to calculate the amount each person needs to pay
+         */
+        
+        let preSplitTotal = Double(totalLabel.text!.replacingOccurrences(of: "$", with: "")) ?? 0
         let numPeople = Int(numPeopleTextField.text!) ?? 1
         let individualBill = preSplitTotal / Double(numPeople)
-        totalPerPersonLabel.text = String(format: "%.2f", individualBill)
+        
+        totalPerPersonLabel.text = String(format: "$%.2f", individualBill)
     }
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        print("view will appear")
-//        // This is a good place to retrieve the default tip percentage from UserDefaults
-//        // and use it to update the tip amount
-//    }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("view did appear")
-//    }
-//
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        print("view will disappear")
-//    }
-//
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("view did disappear")
-//    }
-    
 }
-
